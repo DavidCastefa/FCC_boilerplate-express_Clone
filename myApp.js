e21875412c1085c9eb67390b7723aa45f7e4ff25
David <DRhoton@vinyacastelldefels.com>
2021-06-29 18:51:29 +0200

    move javascript to myApp

--- myApp.js
+++ myApp.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 
+// app.get("/", (req, res) => res.send("Hello Express"));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
+app.use("/public", express.static(__dirname + "/public"));
 
 
 

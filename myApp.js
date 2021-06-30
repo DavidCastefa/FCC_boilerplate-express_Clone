44d73c3ead78c553d9a0a32133840f103f7157a3
David <DRhoton@vinyacastelldefels.com>
2021-06-30 17:26:51 +0200

    Add install dotenv, require dotenv, add .env file and check 'HELLO JSON'

--- myApp.js
+++ myApp.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
+
 console.log("Hello World");
 
 // app.get("/", (req, res) => res.send("Hello Express"));
 
+console.log(process.env.MESSAGE_STYLE, "= message style")
+
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/json", (req, res) => res.json({message: "Hello json"}));
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({message: "HELLO JSON"})
+  } else {
+    res.json({message: "Hello json"})
+  }
+});
 
 module.exports = app;
